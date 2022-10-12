3f0301cd2baf20d0308c6176f9433ba18b7e7e44
jprakash <jprakashjena100@gmail.com>
2022-10-12 15:21:04 +0530

    New Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassComponent } from './students/class/class.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "class", component: ClassComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
